a888dded6d576ac3c935ae0e8fb1efeb5229da35
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-11-30 16:24:26 +0000

    Add project files.

---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
+public class Solution
+{
+    public static void Main()
+    {
+        var test = new Solution();
+        Console.WriteLine(test.RomanToInt("MCMXCIV"));
+    }
+
+    public int RomanToInt(string s)
+    {
+        var c = s.ToCharArray();
+        int result = 0;
+        var roman = new Dictionary<char, int>()
+        {
+            { 'I',1 },{'V',5}, {'X',10}, {'L',50},{'C',100},{'D',500},{'M',1000},
+        };
+
+        for (int i = 0; i < c.Length; i++)
+        {
+            if (i + 1 < c.Length && roman[c[i]] < roman[c[i + 1]]) result -= roman[c[i]];
+            else result += roman[c[i]];
+        }
+
+        return result;
+    }
+}
+
